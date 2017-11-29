42d2c85ed0b00f0a608300e9fa575b444541f3f9
Craig Orcutt <craigaorcutt@gmail.com>
2017-11-29 09:03:42 -0600

    added variable and if/else statement to allow a more universal answer to the problem no matter the length of either array

--- reindeer.js
+++ reindeer.js
@@ -1,21 +1,42 @@
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
-var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen","Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 var colorsIndex = colors.length;
 var reindeerIndex = reindeer.length;
 
 
 var hohohoElement = document.getElementById("reindeer");
 
-function coloredReindeer(colors, reindeer) {
-        for (let i = 0; i < reindeer.length; i++) {
-        hohohoElement.innerHTML+= "<p>" + colors[i] + " " + reindeer[i] + "</p>"
-        };
-    
-} 
+// function coloredReindeer(colors, reindeer) {
+//         if () {
+//                 for (let i = 0; i < reindeer.length; i++) {
+//                 hohohoElement.innerHTML+= "<p>" + colors[i] + " " + reindeer[i] + "</p>"
+//                 };
+//         };
+//         else {
 
-coloredReindeer(colors,reindeer);
+//         }
+    
+// } 
+
+// coloredReindeer(colors,reindeer);
 // am i creating a div called "reindeer"?
 // how do i pull from two different arrays and put them next to each other?
 // .innerhtml? += "<p>colors[i]? + " " + reindeer[i]? + </p>"???
 
 // if statement to find out when an array is shorter?
+
+function coloredReindeer(colors, reindeer) {
+        let shortestArr = [];
+        if (reindeerIndex < colorsIndex) {
+                shortestArr = reindeer;
+        }
+        else {
+                shortestArr = colors;
+        }
+        for (let i = 0; i < shortestArr.length; i++) {
+        hohohoElement.innerHTML+= "<p>" + colors[i] + " " + reindeer[i] + "</p>"
+        };
+    
+} 
+
+ coloredReindeer(colors,reindeer);
